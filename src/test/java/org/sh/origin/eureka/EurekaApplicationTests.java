9d7a60224acffa02e9a6c81b013bc46272bec72f
施浩 <shihao@xsyxsc.com>
2019-02-11 14:15:12 +0800

    no message

---
+++ src/test/java/org/sh/origin/eureka/EurekaApplicationTests.java
@@ -0,0 +1,19 @@
+package org.sh.origin.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!");
+	}
+
+}
+
